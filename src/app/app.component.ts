5d6eaa38c33ce131d79287c373eaaac0497ed4d6
Direksiya <4.sqdep@gmail.com>
2024-09-13 14:37:27 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SpreadsheetAllModule} from "@syncfusion/ej2-angular-spreadsheet";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    SpreadsheetAllModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import {BrowserModule} from "@angular/platform-browser";
+
+import {NgModule} from "@angular/core";
+import {SpreadsheetAllModule} from "@syncfusion/ej2-angular-spreadsheet";
+
+import {AppComponent} from "./app.component";
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    BrowserModule,
+    SpreadsheetAllModule,
+  ],
+  providers: [],
+  bootstrap: []
+})
+
+export class AppModule {
+
+}
